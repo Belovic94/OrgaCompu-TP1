3fd0ed3ea1a1c89aafd7c38d98d4dbaddf764f4f
VictorBelosevich <Victor.belosevich@despegar.com>
2019-10-11 18:54:41 -0300

    Add ant_engine_jumps.S break Seg Fault

--- tp1-q2-2019-src/src/asm-helpers.c
+++ tp1-q2-2019-src/src/asm-helpers.c
@@ -1,7 +1,14 @@
 #include "builders.h"
+#include "ant_engine.h"
 
 void 
-doPanic(char *s)
+doPanic(char *s, orientation_t o)
 {
   panic(s);
+}
+
+void 
+adjust_asm(uint32_t* oldPosition, uint32_t newPosition, uint32_t limit)
+{
+  adjust(oldPosition, newPosition, limit);
 }
